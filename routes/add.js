352a0f9255628844e7c3768cf3774976f097f095
Kate Van Donge <katemvd@gmail.com>
2017-02-09 11:03:35 -0800

    done

--- routes/add.js
+++ routes/add.js
@@ -3,8 +3,8 @@ var data = require("../data.json");
 exports.addFriend = function(req, res) {    
   var name = req.query.name;
   var description = req.query.description; 
-  var obj = {"name": name, "description": description, "imageURL": "http://lorempixel.com/400/400/people"};
-  console.log(obj);
-  data.friends.push(obj);
+  var userobj = {"name": name, "description": description, "imageURL": "http://lorempixel.com/400/400/people"};
+  console.log(userobj);
+  data.friends.push(userobj);
   res.render('index', data);
  }
